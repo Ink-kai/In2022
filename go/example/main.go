cf13b3ca1e6d1f641fd4527497fbfa7f4cc31d43
ink <ink_kai@163.com>
2022-05-28 18:17:36 +0800

    初始化BlogApi项目

--- go/example/main.go
+++ go/example/main.go
@@ -1,35 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
 
-type Blog struct {
-	Title   string
-	Content string
-	Author  string
-	Remark  string
-}
-type B_interface interface {
-	Insert(data map[string]interface{}) error
-}
-
-func (b *Blog) Insert(data map[string]interface{}) error {
-	blog := b
-	blog.Title = data["title"].(string)
-	blog.Content = data["content"].(string)
-	blog.Author = data["author"].(string)
-	blog.Remark = data["remark"].(string)
-	return nil
-}
 func main() {
-	fmt.Printf("---------------------interface Start--------------------\n")
-	var b B_interface = &Blog{}
-	data := map[string]interface{}{
-		"title":   "你好",
-		"author":  "Ink",
-		"content": "内容",
-		"remark":  "备注",
+	r := strings.NewReader("some io.Reader stream to be read\n")
+	_, err := io.Copy(os.Stdout, r)
+	if err != nil {
+		log.Fatal(err)
 	}
-	b.Insert(data)
-	fmt.Printf("%+v\n", b)
-	fmt.Printf("\n---------------------interface End----------------------")
+	fmt.Printf("%v", err)
+	// Output:
+	// some io.Reader stream to be read
 }
--- go/mysqlStore/utils/logger/logger.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "github.com/wonderivan/logger"
-
-func InitLogger() {
-	filepath := ""
-	// 通过配置文件配置
-	logger.SetLogger(filepath)
-}
